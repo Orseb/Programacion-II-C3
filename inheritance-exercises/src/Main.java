22bb0ec12481064ea25433a5dcc75fc171e571be
Seba Ortiz <sebious.ortizx2@gmail.com>
2024-03-15 23:20:32 -0300

    ejercicios de herencia agregados

---
+++ inheritance-exercises/src/Main.java
@@ -0,0 +1,14 @@
+import exercise1.firstExercise;
+import exercise2.secondExercise;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("First exercise:");
+        firstExercise.main(args);
+
+        System.out.println("\n======================================\n");
+
+        System.out.println("Second exercise:");
+        secondExercise.main(args);
+    }
+}
